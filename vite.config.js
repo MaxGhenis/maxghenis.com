4e60e29b4842eb1ebe5c11b39574d977c1a384ff
Max Ghenis <mghenis@gmail.com>
2024-12-31 23:35:48 -0500

    fixes

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: "**/*.{jsx,js}",
+      babel: {
+        plugins: [],
+        babelrc: false,
+        configFile: false,
+      },
+    })
+  ],
   base: '/',
   server: {
-    port: 3000,
+    port: 3000
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,
-  },
+    sourcemap: true
+  }
 })
