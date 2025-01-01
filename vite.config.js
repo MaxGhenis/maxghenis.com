dabde29d6fdbd60eff301eccbb4dbe01a32d84b0
Max Ghenis <mghenis@gmail.com>
2024-12-31 23:40:37 -0500

    add react plugin

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      include: "**/*.{jsx,js}",
-      babel: {
-        plugins: [],
-        babelrc: false,
-        configFile: false,
-      },
-    })
-  ],
+  plugins: [react()],
   base: '/',
-  server: {
-    port: 3000
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
